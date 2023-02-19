75254ea90234846a6cc0a07b87b1323aebfeb4e8
Mohamed Nouri <mohamed.nouri@hellofresh.com>
2023-02-19 16:04:45 +0100

    Add base BaseGneratedCode.swift

---
+++ Plugins/GenerateAssetResources/Extensions/BaseGneratedCode.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+let baseGneratedMessage = "// This file was generated automatically by GenerateAssetResources plugin, Please don't edit"
+
+let baseGneratedCode = """
+            import Foundation
+            import SwiftUI
+            
+            \(baseGneratedMessage)
+            \n
+            """
