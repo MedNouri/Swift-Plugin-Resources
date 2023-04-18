066ee56057821d4765ede361a5d07a258d7503b3
Mohamed Nouri <mohamed.nouri@hellofresh.com>
2023-04-19 00:11:12 +0200

    PackageMetadata Add base GeneratePackageMetadata  CommandPlugin

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 let pluginName = "GenerateAssetResources"
 let description = "Generate Asset Resources"
 let permissionReason = "The plugin writes the generaterated assets swift file"
+let packageMetadataPluginName = "GeneratePackageMetadata"
+let packageMetadataPluginDescription = "Generate Package Metadata"
+let packageMetadataPluginPermissionReason = "The plugin writes the generaterated README.md file"
 
 let package = Package(
   name: "SwiftPluginResources",
@@ -15,6 +18,10 @@ let package = Package(
     .plugin(
       name: "AssetResources",
       targets: [pluginName]
+    ),
+    .plugin(
+      name: "GeneratePackageMetadata",
+      targets: [packageMetadataPluginName]
     )
   ],
 
@@ -28,6 +35,18 @@ let package = Package(
         ),
         permissions: [
           .writeToPackageDirectory(reason: permissionReason)
+        ]
+      )
+    ),
+    .plugin(
+      name: packageMetadataPluginName,
+      capability: .command(
+        intent:  .custom(
+            verb: "generate-package-metadata",
+            description: description
+          ),
+        permissions: [
+          .writeToPackageDirectory(reason: packageMetadataPluginPermissionReason)
         ]
       )
     )
---
+++ Plugins/GeneratePackageMetadata/GeneratePackageMetadata.swift
@@ -0,0 +1,70 @@
+import Foundation
+import PackagePlugin
+
+@main
+struct GeneratePackageMetadata: CommandPlugin {
+    func performCommand(context: PackagePlugin.PluginContext, arguments: [String]) async throws {
+        // Package Header
+        var gneratedCode = getPackageBaseInformations(package: context.package)
+        
+        
+        try writeToFile(
+            contents: gneratedCode
+        )
+    }
+    
+}
+
+extension GeneratePackageMetadata {
+    /// Returns the package base header informations
+    /// - Parameter package: PackagePlugin
+    /// - Returns: Base informations
+    func getPackageBaseInformations(package: PackagePlugin.Package) -> String {
+        """
+        # Package: \(package.displayName) 📦
+        -  ToolsVersion: *\(package.toolsVersion)*
+        -  Origin: *\(package.origin)*
+        -  Directory: *\(package.directory.stem)*
+        -  Dependencies Count: *\(package.dependencies.count)*
+        -  Products Count: *\(package.products.count)*
+        -  Targets Count: *\(package.targets.count)*
+        \n
+        """
+    }
+    
+    /// Runs a Git Command
+    /// - Parameter arguments: Git arguments
+    /// - Returns: outputData as String using .utf8 encoding
+    func runGitCommand(arguments: [String]) throws -> String? {
+        // Create a Process instance for the git command
+        let gitProcess = Process()
+        gitProcess.executableURL = URL(fileURLWithPath: "/usr/bin/git")
+
+        // Set up the standard output pipe to capture the command output
+        let outputPipe = Pipe()
+        gitProcess.standardOutput = outputPipe
+
+        // Launch the git command
+        gitProcess.arguments = arguments
+        try gitProcess.run()
+
+        // Read the output from the standard output pipe
+        let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+        return String(data: outputData, encoding: .utf8)
+    }
+}
+
+extension GeneratePackageMetadata{
+    /// writes string contents to README.md using .utf8 encoding
+    /// - Parameters:
+    ///   - contents: String contents
+    ///   - file: The file name
+    ///   - target: The Source Module Target
+    private func writeToFile(contents: String) throws{
+        try contents.write(
+            toFile: "README.md",
+            atomically: true,
+            encoding: .utf8
+        )
+    }
+}
